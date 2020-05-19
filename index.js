9a6095147e6c4aaa06535596025f48841e2be6a9
hasannirob307875 <64268615+hasannirob307875@users.noreply.github.com>
2020-05-19 22:40:08 +0600

    added html ,css and js code

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var head=document.querySelector("#head");
+var pic=document.querySelector("#photo");
+
+function text1(){
+
+   
+    pic.src ="love.jpg";
+    head.innerHTML="I love you too"
+}
+var head2=document.querySelector("#head2");
+var pic2=document.querySelector("#photo2");
+
+function text2(){
+
+   pic2.src="sad.jpg";
+   head2.innerHTML="But it doesn’t matter, I love you and I will. Stay with me forever, please."
+}
